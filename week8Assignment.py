4d5d81bca1cec5ec6fdbd88bebfcd40795f9ca86
Xiyuan <xliu1@tcd.ie>
2026-04-16 07:34:09 +0100

    Q1(d) completed

--- week8Assignment.py
+++ week8Assignment.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 # Q1 (a), (b)
 def f(x): # Objective function
@@ -57,3 +58,32 @@ def project_X2(z, num_repeats=10): # Repeated projections onto X2 = {x1 >= 0.5, 
     return x
 
 X2, F2 = projected_gradient_descent(f=f, grad_f=grad_f, project=project_X2, x0=x0, alpha=0.1, num_iters=60)
+
+# Q1 (d)
+x1_vals = np.linspace(0.0, 3.0, 400) # (I) Contour plot of f(x1,x2) with feasible region and trajectory
+x2_vals = np.linspace(0.0, 4.0, 400)
+X1_grid, X2_grid = np.meshgrid(x1_vals, x2_vals)
+F_grid = (X1_grid - 1.2)**2 + 2 * (X2_grid - 2.5)**2 + 0.4 * X1_grid * X2_grid
+
+plt.contour(X1_grid, X2_grid, F_grid, levels=25)
+plt.plot(X[:, 0], X[:, 1], marker='o', markersize=3)
+plt.plot(x0[0], x0[1], 'o')
+plt.plot([0.5, 2.5, 2.5, 0.5, 0.5], [0.5, 0.5, 3.5, 3.5, 0.5], '--')
+plt.xlabel('$x_1$')
+plt.ylabel('$x_2$')
+plt.xlim(0.0, 3.0)
+plt.ylim(0.0, 4.0)
+plt.gca().set_aspect('equal')
+plt.show()
+
+plt.plot(F) # (II) f(x_t) versus iteration
+plt.xlabel('#iteration')
+plt.ylabel('$f(x^{(t)})$')
+plt.show()
+
+plt.plot(X[:, 0], label='$x_1^{(t)}$') # (III) x1_t and x2_t versus iteration
+plt.plot(X[:, 1], label='$x_2^{(t)}$')
+plt.xlabel('#iteration')
+plt.ylabel('value')
+plt.legend()
+plt.show()
